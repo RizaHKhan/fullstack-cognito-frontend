a6bf4a392cce18f418df9e0e03ad55f4f99dfdee
khanr <khanriza@gmail.com>
2024-10-10 20:21:16 -0400

    First commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <h1 class="text-red-200">App</h1>
+    <p class="line-clamp-3 md:line-clamp-none">
+      Et molestiae hic earum repellat aliquid est doloribus delectus. Enim illum odio porro ut omnis
+      dolor debitis natus. Voluptas possimus deserunt sit delectus est saepe nihil. Qui voluptate
+      possimus et quia. Eligendi voluptas voluptas dolor cum. Rerum est quos quos id ut molestiae fugit.
+    </p>
+    <select>
+      <option>Yes</option>
+      <option>No</option>
+      <option>Maybe</option>
+    </select>
+
+    <select class="appearance-none">
+      <option>Yes</option>
+      <option>No</option>
+      <option>Maybe</option>
+    </select>
+    <div class="cursor-pointer">
+      Hover me
+    </div>
+
+
+  </div>
+
+</template>
+
+<style scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+import colors from 'tailwindcss/colors'
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
+    colors: {
+      transparent: 'transparent',
+      gray: colors.gray,
+      red: colors.red,
+      blue: colors.sky,
+      yellow: colors.amber
+    }
+  },
+  plugins: [],
+}
