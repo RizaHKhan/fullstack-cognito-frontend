9ee635546e4d6075e39871a73804b82a76b3548a
khanr <khanriza@gmail.com>
2024-10-12 13:46:40 -0400

    Updating app

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,12 @@
 </script>
 
 <template>
-  <div class="container mx-auto">
-    <h1 class="text-red-200">App</h1>
-    <p class="line-clamp-3 md:line-clamp-none">
-      Et molestiae hic earum repellat aliquid est doloribus delectus. Enim illum odio porro ut omnis
-      dolor debitis natus. Voluptas possimus deserunt sit delectus est saepe nihil. Qui voluptate
-      possimus et quia. Eligendi voluptas voluptas dolor cum. Rerum est quos quos id ut molestiae fugit.
-    </p>
-    <select>
-      <option>Yes</option>
-      <option>No</option>
-      <option>Maybe</option>
-    </select>
-
-    <select class="appearance-none">
-      <option>Yes</option>
-      <option>No</option>
-      <option>Maybe</option>
-    </select>
-    <div class="cursor-pointer">
-      Hover me
+  <div class="h-screen bg-gradient-to-b from-gray-100 to-gray-300">
+    <div class="flex flex-col items-center justify-center h-full">
+      <h1 class="text-4xl font-bold text-gray-800">Hello Vite + Vue 3 + Tailwind CSS</h1>
+      <p class="text-gray-600">Edit <code>App.vue</code> and save to test HMR updates.</p>
     </div>
-
-
   </div>
-
 </template>
 
 <style scoped></style>
